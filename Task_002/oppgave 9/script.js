132c08383b5f13877384a6f3b5cd26c757c5b097
hyperit123 <117358229+hyperit123@users.noreply.github.com>
2025-09-03 14:20:17 +0200

    rockers
    proplem 9 stated and  more file things

---
+++ Task_002/oppgave 9/script.js
@@ -0,0 +1,31 @@
+let score = 0
+let btnA = document.getElementById("A")
+let btnB = document.getElementById("B")
+let btnC = document.getElementById("C")
+let btnD = document.getElementById("D")
+let curent = 1
+let questions = [
+    {
+        question: "What is Dom Cobb's main profession in the movie?",
+        answer: "A"
+    }
+]
+
+function loadQuestion() {
+    if(curent > ) {
+        document.getElementByclassName("question").innerHTML = "congrats! you finished the quiz. Her is your score: " + score
+    }
+}
+
+btnA.onclick = () => {
+    
+}
+btnB.onclick = () => {
+    
+}
+btnC.onclick = () => {
+    
+}
+btnD.onclick = () => {
+    
+}
